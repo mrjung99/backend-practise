832cfe190adfc7ea003a7c52d59320b2e636308f
zeroCool <jungdaulat99@gmail.com>
2026-01-08 23:53:14 +0545

    mongoose

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "./config.env" });
 
 //this app must be imported before dotenv
 import { app } from "./app.js";
+
+//connect to mongodb
+mongoose
+  .connect(process.env.CONN_STR)
+  .then((conn) => {
+    // console.log(conn);
+    console.log("DB connection successful...");
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err.message);
+    process.exit(1); // Exit process on connection failure
+  });
 
 // create a server
 const PORT = process.env.PORT || 3000;
