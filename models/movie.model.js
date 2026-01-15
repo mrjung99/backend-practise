6d1499425022b722df5100575eaa79c36272c11f
zeroCool <jungdaulat99@gmail.com>
2026-01-15 22:04:45 +0545

    middleware: query middleware

--- models/movie.model.js
+++ models/movie.model.js
@@ -59,7 +59,7 @@ movieSchema.virtual("durationInHours").get(function () {
   return this.duration / 60;
 });
 
-//document middleware
+//!document middleware
 //works with save(), remove() not with findByIdAndUpdate() etc
 movieSchema.pre("save", function () {
   this.createdBy = "Daulat";
@@ -69,6 +69,25 @@ movieSchema.post("save", function (doc) {
   const content = `${doc.name} is created by ${doc.createdBy}`;
   // console.log(`${doc.name} is created by ${doc.createdBy}`);
   // here the {flag:"a"} is used to append the new log on previous log on the file if we dont do that it will rewrite the whole file
+  fs.writeFileSync("./log/log.txt", content, { flag: "a" }, (err) => {
+    console.log(err);
+  });
+});
+
+//! query middleware
+// here this find query works with getAllMovies() controller but not with the patch and update bcoz  findByIdAndUpdate is different then find query so we use regex /^find/ so it will works for all the query starting with find.
+movieSchema.pre(/^find/, async function () {
+  console.log("query middleware is called");
+  this.find({ releaseYear: { $lte: new Date().getFullYear() } });
+  this.startTime = Date.now();
+});
+
+movieSchema.post(/^find/, function () {
+  this.endTime = Date.now();
+  const content = `\nQuery took ${
+    this.endTime - this.startTime
+  } ms to fetch document.`;
+
   fs.writeFileSync("./log/log.txt", content, { flag: "a" }, (err) => {
     console.log(err);
   });
